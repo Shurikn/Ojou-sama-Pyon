d5bf09d7ecb17a3e710ca4424cc7f4fffb7c5843
Shurikn <doshurikn@gmail.com>
2018-05-11 14:10:27 +0900

    animated emoji support

--- ojousama.py
+++ ojousama.py
@@ -3,6 +3,7 @@ import asyncio
 import re
 
 REGEX = "<:[a-zA-Z0-9_]{2,}:(\d+)>"
+ANIMATED_REGEX="<a:[a-zA-Z0-9_]{2,}:(\d+)>"
 
 client = discord.Client()
 
@@ -29,5 +30,17 @@ async def on_message(message):
             i+=1
             if i >= 3:
                 break
-        
+    pattern = re.compile(ANIMATED_REGEX)
+    i = 0
+    already_posted_emoji = []
+    for image_id in re.findall(pattern, message_text):
+        if image_id not in already_posted_emoji:
+            already_posted_emoji.append(image_id)
+            url = "https://cdn.discordapp.com/emojis/" + image_id + ".gif"
+            embed = discord.Embed()
+            embed.set_image(url=url)
+            await client.send_message(message.channel, embed=embed)
+            i += 1
+            if i >= 3:
+                break
 client.run('TOKEN')
